40cf52707bc4fe3c761ee865bbc591df212d0702
GayeKORDACI <kordacig@gmail.com>
2022-05-30 21:58:45 +0300

    initial commit

---
+++ Week2-GayeKORDACI/Week2-GayeKORDACI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Week2-GayeKORDACI
+//
+//  Created by Gaye Kordacı Deprem on 30.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

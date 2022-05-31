2a92d86c932d18f79377ea7b3f88edfe56da7a84
GayeKORDACI <kordacig@gmail.com>
2022-05-31 18:45:26 +0300

    Second View Controller initial commit

---
+++ Week2-GayeKORDACI/Week2-GayeKORDACI/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  Week2-GayeKORDACI
+//
+//  Created by Gaye Kordacı Deprem on 31.05.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
